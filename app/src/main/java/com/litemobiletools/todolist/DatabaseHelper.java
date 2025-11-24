5be64335eaa23f05c4e31972ce8272a2ffd475b4
Md. Shahinur Islam <shahinalam6644@gmail.com>
2025-11-24 17:39:44 +0600

    update

--- app/src/main/java/com/litemobiletools/todolist/DatabaseHelper.java
+++ app/src/main/java/com/litemobiletools/todolist/DatabaseHelper.java
@@ -12,13 +12,14 @@ import java.util.Locale;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
-    private static final String DATABASE_NAME = "mydatabase.db";
+    private static final String DATABASE_NAME = "todolist.db";
     private static final int DATABASE_VERSION = 1;
-    private static final String TABLE_ITEMS = "items";
+    private static final String TABLE_ITEMS = "todo_list";
     private static final String COLUMN_ID = "id";
     private static final String COLUMN_NAME = "name";
     private static final String COLUMN_DATE = "date_Time";
     private static final String COLUMN_CHECKED = "is_checked";
+    private static final String COLUMN_CAT = "cat_name";
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -28,7 +29,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         String CREATE_TABLE = "CREATE TABLE " + TABLE_ITEMS + " (" +
                 COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                COLUMN_NAME + " TEXT, " + COLUMN_DATE + " TEXT," + COLUMN_CHECKED + " INTEGER)";
+                COLUMN_NAME + " TEXT, " + COLUMN_DATE + " TEXT," + COLUMN_CHECKED + " INTEGER," + COLUMN_CAT + " TEXT)";
         db.execSQL(CREATE_TABLE);
     }
 
@@ -38,18 +39,21 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         onCreate(db);
     }
 
-    public long insertItem(String name) {
+    public long insertItem(String name, String cat_name) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues values = new ContentValues();
         values.put(COLUMN_NAME, name);
-        // Get the current date
-        //String currentDate = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
-        //values.put(COLUMN_DATE, currentDate);
         // Get the current date and time
         String currentDateAndTime = new SimpleDateFormat("hh:mma d-MMM-yy", Locale.getDefault()).format(new Date());
         values.put(COLUMN_DATE, currentDateAndTime);
         values.put(COLUMN_CHECKED, 0);
+        values.put(COLUMN_CAT, cat_name);
         return db.insert(TABLE_ITEMS, null, values);
+    }
+
+    public Cursor getItemByCat(String id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        return db.query(TABLE_ITEMS, null, COLUMN_CAT + "=?", new String[]{String.valueOf(id)}, null, null, null);
     }
 
     public Cursor getAllItems() {
--- app/src/main/java/com/litemobiletools/todolist/TaskList.java
+++ app/src/main/java/com/litemobiletools/todolist/TaskList.java
@@ -1,6 +1,12 @@
 package com.litemobiletools.todolist;
 
+import android.annotation.SuppressLint;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +15,56 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class TaskList extends AppCompatActivity {
-
+    public DatabaseHelper myDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_task_list);
+        myDatabase = new DatabaseHelper(this);
+    }
+    public void create(View view) {
+        // Insert a new item in the database
+        String name = "Test";
+        String cat_name = "general_list";
+        long newItemId = myDatabase.insertItem(name, cat_name);
+        if (newItemId != -1) { // Check if insertion was successful
+            // Refresh the list
+            loadDataFromDatabase(cat_name);
+        } else {
+            Toast.makeText(this, "Error inserting item", Toast.LENGTH_SHORT).show();
+        }
+    }
+    // main for loop main content
+    @SuppressLint("Range")
+    public void loadDataFromDatabase(String cat_name){
+        Cursor cursor = myDatabase.getItemByCat(cat_name);
+        if (cursor.getCount() == 0) {
+            Toast.makeText(getApplicationContext(), "Not Found", Toast.LENGTH_LONG).show();
+        } else {
+            if (cursor.moveToFirst()) {
+                do {
+                    String varId = cursor.getString(cursor.getColumnIndex("Id"));
+                    String  name = cursor.getString(cursor.getColumnIndex("name"));
+                    //TEXT VIEW
+                    CheckBox checkboxtext = new CheckBox(this);
 
+                    checkboxtext.setText(name);
+                    checkboxtext.setTextSize(16);
+                    checkboxtext.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+
+                            String dataID = varId;
+                        }
+                    });
+
+                    LinearLayout ll = (LinearLayout)findViewById(R.id.taskListContainer);
+                    LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                    checkboxtext.setLayoutParams(lp);
+                    ll.addView(checkboxtext, lp);
+                } while (cursor.moveToNext());
+            }
+            cursor.close();
+        }
     }
 }
